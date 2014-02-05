eb8b3ac8ef037c0acc7839ba8f40cbea26b2e19f
Jason Fox <jasonrobertfox@gmail.com>
2014-02-05 16:37:28 -0300

    Initial commit.

---
+++ week1/Percolation.java
@@ -0,0 +1,121 @@
+public class Percolation
+{
+
+  private static final int OPEN_FLAG = 1;
+  private static final int CONNECTED_TO_BOTTOM_FLAG = 2;
+
+  private int width;
+  private int size;
+  private int[] grid;
+  private int virtualSite;
+  private QuickFindUF unionFind;
+  private boolean percolates;
+
+  public Percolation(int N)
+  {
+    // Set up the grid construct
+    width = N;
+    size = width * width;
+    grid = new int[size + 1];
+    grid[size] = 1;
+    virtualSite = size;
+    unionFind = new QuickFindUF(size + 1);
+  }
+
+  public boolean percolates()
+  {
+    return percolates;
+  }
+
+  public boolean isOpen(int row, int col)
+  {
+    return isIndexOpen(indexFromRowCol(row, col));
+  }
+
+  public boolean isFull(int row, int col)
+  {
+    return unionFind.connected(indexFromRowCol(row, col), virtualSite);
+  }
+
+  public void open(int row, int col)
+  {
+    int index = indexFromRowCol(row, col);
+    grid[index] = OPEN_FLAG;
+
+    // union to the bottom virtual site if in the bottom row
+    if (index >= size - width) {
+      connectToBottom(index);
+    }
+
+    // union to top virtual site if in the top row
+    if (index < width) {
+      joinSite(index, virtualSite);
+    }
+
+    // union to top if top is open
+    int top = index - width;
+    if (top >= 0 && isIndexOpen(top)) {
+      joinSite(index, top);
+    }
+
+    // union to below if below is open
+    int below = index + width;
+    if (below < size && isIndexOpen(below)) {
+      joinSite(index, below);
+    }
+
+    // union to right if right is open
+    int right = index + 1;
+    if (right % width != 0 && isIndexOpen(right)) {
+      joinSite(index, right);
+    }
+
+    // union to left if left is open
+    int left = index - 1;
+    if (index % width != 0 && isIndexOpen(left)) {
+      joinSite(index, left);
+    }
+
+    if (isConnectedToBottom(index) && unionFind.connected(index, virtualSite)) {
+      percolates = true;
+    }
+
+  }
+
+  private void connectToBottom(int index)
+  {
+    grid[index] = CONNECTED_TO_BOTTOM_FLAG;
+  }
+
+  private boolean isConnectedToBottom(int index)
+  {
+    return grid[index] == CONNECTED_TO_BOTTOM_FLAG;
+  }
+
+  private int indexFromRowCol(int row, int col)
+  {
+    int index = width * (row - 1) + (col - 1);
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException("Coordinate out of bounds.");
+    }
+    return index;
+  }
+
+  private boolean isIndexOpen(int index)
+  {
+    return grid[index] > 0;
+  }
+
+  private void joinSite(int siteIndex, int neighborIndex)
+  {
+    if (isIndexOpen(neighborIndex)) {
+      unionFind.union(siteIndex, neighborIndex);
+
+      if (isConnectedToBottom(siteIndex) || isConnectedToBottom(neighborIndex)) {
+        connectToBottom(siteIndex);
+        connectToBottom(unionFind.find(siteIndex));
+      }
+    }
+  }
+
+}
---
+++ week1/PercolationTest.java
@@ -0,0 +1,179 @@
+import junit.framework.TestCase;
+
+/**
+ * A JUnit test case class. Every method starting with the word "test" will be
+ * called when running the test with JUnit.
+ */
+public class PercolationTest extends TestCase
+{
+
+  private int size;
+  private Percolation p;
+
+  @Override
+  public void setUp()
+  {
+    size = 3;
+    p = new Percolation(size);
+  }
+
+  public void testInitialize()
+  {
+    assertTrue(p instanceof Percolation);
+  }
+
+  public void testAllSitesShouldBeNotOpenByDefault()
+  {
+    for (int row = 1; row <= size; row++) {
+      for (int col = 1; col <= size; col++) {
+        assertFalse(p.isOpen(row, col));
+      }
+    }
+  }
+
+  public void testCanOpenASite()
+  {
+    assertFalse(p.isOpen(2, 2));
+    p.open(2, 2);
+    assertTrue(p.isOpen(2, 2));
+  }
+
+  public void testNotFullByDefault()
+  {
+    assertNotFull(1, 1);
+  }
+
+  public void testHandleExceptionForFirstOpenOutOfBound()
+  {
+    try {
+      p.open(4, 2);
+      fail("Expected IndexOutOfBoundsException");
+    } catch (IndexOutOfBoundsException e) {
+      assertTrue(e instanceof IndexOutOfBoundsException);
+    }
+  }
+
+  public void testOpenTopSiteIsFull()
+  {
+    p.open(1, 1);
+    assertTrue(p.isOpen(1, 1));
+    assertFull(1, 1);
+  }
+
+  public void testOpenBottomSiteIsNotFull()
+  {
+    p.open(3, 1);
+    assertNotFull(1, 1);
+  }
+
+  public void testPercolatesOnSmallGrid()
+  {
+    Percolation ptest = new Percolation(1);
+    ptest.open(1, 1);
+    assertTrue(ptest.isOpen(1, 1));
+    assertTrue(ptest.isFull(1, 1));
+    assertTrue(ptest.percolates());
+  }
+
+  public void testTwoConnectedSitesAreFull()
+  {
+    p.open(2, 1);
+    p.open(1, 1);
+    assertFull(2, 1);
+  }
+
+  public void testConnectingUpFromBottomRow()
+  {
+    Percolation ptest = new Percolation(2);
+    ptest.open(1, 1);
+    ptest.open(2, 1);
+    assertTrue(ptest.isFull(2, 1));
+    assertTrue(ptest.percolates());
+  }
+
+  public void testMiddleTopAndBottomConnecting()
+  {
+    p.open(1, 1);
+    p.open(3, 1);
+    p.open(2, 1);
+    assertFull(3, 1);
+    assertPercolates();
+  }
+
+  public void testRightConnecting()
+  {
+    p.open(1, 2);
+    p.open(2, 2);
+    p.open(3, 1);
+    assertNotPercolates();
+    assertNotFull(3, 1);
+    p.open(2, 1);
+    assertFull(2, 1);
+    assertPercolates();
+  }
+
+  public void testLeftConnecting()
+  {
+    p.open(1, 2);
+    p.open(2, 2);
+    p.open(3, 3);
+    assertNotPercolates();
+    assertNotFull(3, 3);
+    p.open(2, 3);
+    assertFull(2, 3);
+    assertPercolates();
+  }
+
+  public void testStrangeConnection()
+  {
+    p.open(3, 1);
+    p.open(2, 2);
+    p.open(1, 3);
+    p.open(3, 2);
+    p.open(2, 3);
+    assertPercolates();
+  }
+
+  public void testComplicatedPercolation()
+  {
+    Percolation ptest = new Percolation(5);
+    int[][] steps = { { 1, 5 }, { 2, 4 }, { 2, 5 }, { 5, 2 }, { 4, 3 },
+        { 4, 2 }, { 2, 2 }, { 3, 4 }, { 3, 3 } };
+    for (int[] step : steps) {
+      ptest.open(step[0], step[1]);
+    }
+    assertTrue(ptest.percolates());
+  }
+
+  public void testThatNoBackwashHappens()
+  {
+    p.open(1, 1);
+    p.open(2, 1);
+    p.open(3, 3);
+    assertNotPercolates();
+    assertNotFull(3, 3);
+    p.open(3, 1);
+    assertPercolates();
+    assertNotFull(3, 3);
+  }
+
+  private void assertPercolates()
+  {
+    assertTrue(p.percolates());
+  }
+
+  private void assertNotPercolates()
+  {
+    assertFalse(p.percolates());
+  }
+
+  private void assertNotFull(int row, int col)
+  {
+    assertFalse(p.isFull(row, col));
+  }
+
+  private void assertFull(int row, int col)
+  {
+    assertTrue(p.isFull(row, col));
+  }
+}
